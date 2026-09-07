a2cbc06048092bc3b61015cc94023e1375ab79cf
jietyue <katayalive@gmail.com>
2026-09-07 21:19:03 +0200

    active border color change

--- dotfiles/hypr/decorations.lua
+++ dotfiles/hypr/decorations.lua
@@ -11,7 +11,7 @@ hl.config({
         border_size = 4,
 
         col = {
-	    active_border   = { colors = {"rgba(1c202bff)", "rgba(ffffffff)"}, angle = 225 },
+	    active_border   = { colors = {"rgba(1c202bff)", "rgba(b1afafff)"}, angle = 225 },
             inactive_border = "rgba(595959aa)",
         },
 
